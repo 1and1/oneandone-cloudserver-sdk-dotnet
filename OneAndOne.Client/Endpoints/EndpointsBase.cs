51e3ee659758ddb1157bd2b8b9d9ba7242d286ad
Ali Bazlamit <ali.bazlamit@hotmail.com>
2015-11-05 18:23:14 +0100

    Project Init.

---
+++ OneAndOne.Client/Endpoints/EndpointsBase.cs
@@ -0,0 +1,20 @@
+﻿using RestSharp;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneAndOne.Client.Endpoints
+{
+    public class EndpointsBase
+    {
+        RestClient restclient;
+        string APIKEY = "f6df680ed41db7940fd0b2a42bddbf98";
+        public EndpointsBase()
+        {
+
+            restclient = new RestClient("Endoiunt");
+        }
+    }
+}
---
+++ OneAndOne.Client/Endpoints/Servers.cs
@@ -0,0 +1,13 @@
+﻿using OneAndOne.Client.Endpoints;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OneAndOne.Client
+{
+    public class Servers : EndpointsBase
+    {
+    }
+}
