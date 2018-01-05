8a7c2d53df2d87146baedbaeebb4584f0a09a005
aajdinov <amel.a.z@gmail.com>
2018-01-05 21:18:58 +0100

    Added SSH keys support (POCO classes, client endpoint, and unit tests).

---
+++ OneAndOne.POCO/Requests/SshKeys/UpdateSshKeyRequest.cs
@@ -0,0 +1,29 @@
+﻿using Newtonsoft.Json;
+
+namespace OneAndOne.POCO.Requests.SshKeys
+{
+    public class UpdateSshKeyRequest
+    {
+        private string name;
+        ///<summary>
+        ///SSH Key name.
+        ///</summary>
+        [JsonProperty(PropertyName = "name")]
+        public string Name
+        {
+            get { return name; }
+            set { name = value; }
+        }
+
+        private string description;
+        ///<summary>
+        ///SSH Key description.
+        ///</summary>
+        [JsonProperty(PropertyName = "description")]
+        public string Description
+        {
+            get { return description; }
+            set { description = value; }
+        }
+    }
+}
