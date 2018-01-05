bd2e6d56c50a36b1888fceaa7bd2d767e4f18996
aajdinov <amel.a.z@gmail.com>
2018-01-06 00:39:23 +0100

    Added block storages support (POCO classes, client endpoint, and unit tests).

--- OneAndOne.Client/OneAndOneClient.cs
+++ OneAndOne.Client/OneAndOneClient.cs
@@ -17,6 +17,7 @@ using OneAndOne.Client.Endpoints.SharedStorages;
 using OneAndOne.Client.Endpoints.Usages;
 using OneAndOne.Client.Endpoints.Users;
 using OneAndOne.Client.Endpoints.Vpn;
+using OneAndOne.Client.Endpoints.BlockStorages;
 using OneAndOne.Client.RESTHelpers;
 using System;
 using System.Collections.Generic;
@@ -352,6 +353,17 @@ namespace OneAndOne.Client
             get
             {
                 return new Common(configuration.ApiUrl, configuration.ApiKey);
+            }
+        }
+
+        /// <summary>
+        /// BlockStorages client
+        /// </summary>
+        public BlockStorages BlockStorages
+        {
+            get
+            {
+                return new BlockStorages(configuration.ApiUrl, configuration.ApiKey);
             }
         }
     }
---
+++ OneAndOne.POCO/Requests/BlockStorages/UpdateBlockStorageRequest.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace OneAndOne.POCO.Requests.BlockStorages
+{
+    public class UpdateBlockStorageRequest
+    {
+        /// <summary>
+        /// Name of the block storage
+        /// </summary>
+        [JsonProperty(PropertyName = "name")]
+        public string Name { get; set; }
+
+        /// <summary>
+        /// Description of the block storage
+        /// </summary>
+        [JsonProperty(PropertyName = "description")]
+        public string Description { get; set; }
+
+        /// <summary>
+        /// Size of the block storage. minimum: 20, maximum: 500, multipleOf: 10
+        /// </summary>
+        [JsonProperty(PropertyName = "size")]
+        public int Size { get; set; }
+    }
+}
