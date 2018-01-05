0259e08929bfd051841d69f13fc27222b79af62c
aajdinov <amel.a.z@gmail.com>
2018-01-06 00:39:23 +0100

    Added block storages support (POCO classes, client endpoint, and unit tests).

---
+++ OneAndOne.Client/Endpoints/BlockStorages/BlockStorages.cs
@@ -0,0 +1,266 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using OneAndOne.Client.RESTHelpers;
+using OneAndOne.POCO.Requests.BlockStorages;
+using OneAndOne.POCO.Response.BlockStorages;
+using RestSharp;
+
+namespace OneAndOne.Client.Endpoints.BlockStorages
+{
+    public class BlockStorages : ResourceBase
+    {
+        public BlockStorages(object _apiUrl = null, object _apiKey = null) 
+            : base(_apiUrl, _apiKey) { }
+
+        #region Basic Operations
+
+        /// <summary>
+        /// Returns a list of your block storages.
+        /// </summary>
+        /// <param name="page">Allows to use pagination. Sets the number of block storages that will be shown in each page.</param>
+        /// <param name="perPage">Current page to show.</param>
+        /// <param name="sort">Allows to sort the result by priority:sort=name retrieves a list of elements ordered by their names.sort=-creation_date retrieves a list of elements ordered according to their creation date in descending order of priority.</param>
+        /// <param name="query">Allows to search one string in the response and return the elements that contain it. In order to specify the string use parameter q: q=My block storage</param>
+        /// <param name="fields">Returns only the parameters requested: fields=id,name,size</param>
+        public List<BlockStoragesResponse> Get(int? page = null, int? perPage = null, string sort = null, string query = null, string fields = null)
+        {
+            try
+            {
+                string requestUrl = "/block_storages?";
+                if (page != null)
+                {
+                    requestUrl += string.Format("&page={0}", page);
+                }
+                if (perPage != null)
+                {
+                    requestUrl += string.Format("&per_page={0}", perPage);
+                }
+                if (!string.IsNullOrEmpty(sort))
+                {
+                    requestUrl += string.Format("&sort={0}", sort);
+                }
+                if (!string.IsNullOrEmpty(query))
+                {
+                    requestUrl += string.Format("&q={0}", query);
+                }
+                if (!string.IsNullOrEmpty(fields))
+                {
+                    requestUrl += string.Format("&fields={0}", fields);
+                }
+                var request = new RestRequest(requestUrl, Method.GET);
+
+                var result = restclient.Execute<List<BlockStoragesResponse>>(request);
+                if (result.StatusCode != HttpStatusCode.OK)
+                {
+                    throw new Exception(result.Content);
+                }
+                return result.Data;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Adds a new block_storage.
+        /// </summary>
+        public BlockStoragesResponse Create(CreateBlockStorageRequest blockStorage)
+        {
+            try
+            {
+                var request = new RestRequest("/block_storages", Method.POST)
+                {
+                    RequestFormat = DataFormat.Json,
+                    JsonSerializer = new CustomSerializer()
+                };
+                request.AddBody(blockStorage);
+
+                var result = restclient.Execute<BlockStoragesResponse>(request);
+                if (result.StatusCode != HttpStatusCode.Accepted && result.StatusCode != HttpStatusCode.Created)
+                {
+                    throw new Exception(result.Content);
+                }
+                return result.Data;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Returns information about a block storage.
+        /// </summary>
+        /// <param name="blockStorageId">Unique block storage's identifier.</param>
+        public BlockStoragesResponse Show(string blockStorageId)
+        {
+            try
+            {
+                var request = new RestRequest("/block_storages/{block_storage_id}", Method.GET);
+                request.AddUrlSegment("block_storage_id", blockStorageId);
+
+                var result = restclient.Execute<BlockStoragesResponse>(request);
+                if (result.StatusCode != HttpStatusCode.OK)
+                {
+                    throw new Exception(result.Content);
+                }
+                return result.Data;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Modifies a block storage.
+        /// </summary>
+        /// <param name="blockStorageId">Unique block storage's identifier.</param>
+        public BlockStoragesResponse Update(UpdateBlockStorageRequest blockStorage, string blockStorageId)
+        {
+            try
+            {
+                var request = new RestRequest("/block_storages/{block_storage_id}", Method.PUT)
+                {
+                    RequestFormat = DataFormat.Json,
+                    JsonSerializer = new CustomSerializer()
+                };
+                request.AddUrlSegment("block_storage_id", blockStorageId);
+                request.AddBody(blockStorage);
+
+                var result = restclient.Execute<BlockStoragesResponse>(request);
+                if (result.StatusCode != HttpStatusCode.Accepted)
+                {
+                    throw new Exception(result.Content);
+                }
+                return result.Data;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Removes a block storage.
+        /// </summary>
+        /// <param name="blockStorageId">Unique block storage's identifier.</param>
+        public BlockStoragesResponse Delete(string blockStorageId)
+        {
+            try
+            {
+                var request = new RestRequest("/block_storages/{block_storage_id}", Method.DELETE)
+                {
+                    RequestFormat = DataFormat.Json,
+                    JsonSerializer = new CustomSerializer()
+                };
+                request.AddUrlSegment("block_storage_id", blockStorageId);
+                request.AddHeader("Content-Type", "application/json");
+
+                var result = restclient.Execute<BlockStoragesResponse>(request);
+                if (result.StatusCode != HttpStatusCode.Accepted)
+                {
+                    throw new Exception(result.Content);
+                }
+                return result.Data;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        #endregion
+
+        #region Block Storages Server Operations
+
+        /// <summary>
+        /// Attaches a server to a block storage.
+        /// </summary>
+        /// <param name="blockStorageId">Unique block storage's identifier.</param>
+        public BlockStoragesResponse CreateServerBlockStorage(BlockStorageServerRequest serverBlockStorage, string blockStorageId)
+        {
+            try
+            {
+                var request = new RestRequest("/block_storages/{block_storage_id}/server", Method.POST)
+                {
+                    RequestFormat = DataFormat.Json,
+                    JsonSerializer = new CustomSerializer()
+                };
+                request.AddUrlSegment("block_storage_id", blockStorageId);
+                request.AddBody(serverBlockStorage);
+
+                var result = restclient.Execute<BlockStoragesResponse>(request);
+                if (result.StatusCode != HttpStatusCode.Created)
+                {
+                    throw new Exception(result.Content);
+                }
+                return result.Data;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Returns information about the server attached to the block storage.
+        /// </summary>
+        /// <param name="blockStorageId">Unique block storage's identifier.</param>
+        public Server GetBlockStorageServer(string blockStorageId)
+        {
+            try
+            {
+                var request = new RestRequest("/block_storages/{block_storage_id}/server", Method.GET);
+                request.AddUrlSegment("block_storage_id", blockStorageId);
+
+                var result = restclient.Execute<Server>(request);
+                if (result.StatusCode != HttpStatusCode.OK)
+                {
+                    throw new Exception(result.Content);
+                }
+                return result.Data;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        /// <summary>
+        /// Detaches a server from a block storage.
+        /// </summary>
+        /// <param name="blockStorageId">Unique block storage's identifier.</param>
+        /// 
+        public BlockStoragesResponse DeleteBlockStorageServer(BlockStorageServerRequest serverBlockStorage, string blockStorageId)
+        {
+            try
+            {
+                var request = new RestRequest("/block_storages/{block_storage_id}/server", Method.DELETE)
+                {
+                    RequestFormat = DataFormat.Json,
+                    JsonSerializer = new CustomSerializer()
+                };
+                request.AddUrlSegment("block_storage_id", blockStorageId);
+                request.AddBody(serverBlockStorage);
+                request.AddHeader("Content-Type", "application/json");
+
+                var result = restclient.Execute<BlockStoragesResponse>(request);
+                if (result.StatusCode != HttpStatusCode.Accepted)
+                {
+                    throw new Exception(result.Content);
+                }
+                return result.Data;
+            }
+            catch
+            {
+                throw;
+            }
+        }
+
+        #endregion
+
+    }
+}
---
+++ OneAndOne.UnitTests/BlockStorage/BlockStoragesTest.cs
@@ -0,0 +1,135 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OneAndOne.Client;
+using OneAndOne.POCO.Requests.BlockStorages;
+using OneAndOne.POCO.Response.BlockStorages;
+using OneAndOne.POCO.Response.Servers;
+
+namespace OneAndOne.UnitTests.BlockStorage
+{
+    [TestClass]
+    public class BlockStoragesTest
+    {
+        static OneAndOneClient client = OneAndOneClient.Instance(Config.Configuration);
+        static BlockStoragesResponse blockStorage;
+        static ServerResponse server;
+
+        [ClassInitialize]
+        static public void ServerAndBlockStorageInit(TestContext context)
+        {
+            var random = new Random();
+            server = Config.CreateTestServer(random.Next(100, 999) + "blockStorage test .net");
+            Config.waitServerReady(server.Id);
+            server = client.Servers.Show(server.Id);
+            CreateBlockStorage();
+        }
+
+        [ClassCleanup]
+        static public void ServerAndBlockStorageClean()
+        {
+            if (blockStorage != null)
+            {
+                Config.WaitBlockStorageReady(blockStorage.Id);
+                DeleteBlockStorage();
+            }
+
+            if (server != null)
+            {
+                Config.waitServerReady(server.Id);
+                client.Servers.Delete(server.Id, false);
+            }
+        }
+
+        public static void CreateBlockStorage()
+        {
+            Config.waitServerReady(server.Id);
+            var random = new Random();
+
+            blockStorage = client.BlockStorages.Create(new CreateBlockStorageRequest()
+            {
+                Name = random.Next(100, 999) + "testBlockStorage",
+                Description = ".NET SDK unit testing",
+                DatacenterId = server.Datacenter.Id,
+                Size = 60
+            });
+
+            Assert.IsNotNull(blockStorage);
+            Assert.IsNotNull(blockStorage.Id);
+            blockStorage = client.BlockStorages.Show(blockStorage.Id);
+            Assert.IsNotNull(blockStorage);
+            Assert.IsNotNull(blockStorage.Id);
+        }
+
+        private static void DeleteBlockStorage()
+        {
+            var result = client.BlockStorages.Delete(blockStorage.Id);
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(result.Id);
+            result = client.BlockStorages.Show(result.Id);
+            Assert.IsTrue(result.State == "REMOVING");
+        }
+
+        [TestMethod]
+        public void GetBlockStorages()
+        {
+            var result = client.BlockStorages.Get();
+
+            Assert.IsNotNull(result);
+            Assert.IsTrue(result.Count > 0);
+        }
+
+        [TestMethod]
+        public void ShowBlockStorage()
+        {
+            var result = client.BlockStorages.Show(blockStorage.Id);
+
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(result.Id);
+        }
+
+        [TestMethod]
+        public void UpdateBlockStorage()
+        {
+            Config.WaitBlockStorageReady(blockStorage.Id);
+            var result = client.BlockStorages.Update(new POCO.Requests.BlockStorages.UpdateBlockStorageRequest()
+            {
+                Description = blockStorage.Description + " - Updated",
+                Name = blockStorage.Name + " - Updated",
+                Size = blockStorage.Size + 10
+            }, blockStorage.Id);
+
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(result.Id);
+
+            var blockStorageResponse = client.BlockStorages.Show(result.Id);
+            Assert.IsNotNull(blockStorageResponse.Id);
+            Assert.AreEqual(result.Description, blockStorageResponse.Description);
+            Assert.AreEqual(result.Size, blockStorageResponse.Size);
+            Assert.AreEqual(result.Name, blockStorageResponse.Name);
+            Config.WaitBlockStorageReady(result.Id);
+        }
+
+        [TestMethod]
+        public void AttachServerToBlockStorage()
+        {
+            var serverBlockStorage = new BlockStorageServerRequest()
+            {
+                ServerId = server.Id
+            };
+
+            var result = client.BlockStorages.CreateServerBlockStorage(serverBlockStorage, blockStorage.Id);
+
+            Assert.IsNotNull(result);
+            Assert.IsNotNull(result.Id);
+        }
+
+        [TestMethod]
+        public void GetBlockStorageServer()
+        {
+            var blockStorageServer = client.BlockStorages.GetBlockStorageServer(blockStorage.Id);
+
+            Assert.IsNotNull(blockStorageServer);
+            Assert.IsNotNull(blockStorageServer.Id);
+        }
+    }
+}
